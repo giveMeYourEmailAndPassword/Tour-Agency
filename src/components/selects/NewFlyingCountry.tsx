415944d68ce1a385aee83b44a604606fe3d17e0f
usisex <vvvvv8206@gmail.com>
2025-02-26 14:41:54 +0500

    bismillah

--- src/components/selects/NewFlyingCountry.tsx
+++ src/components/selects/NewFlyingCountry.tsx
@@ -7,22 +7,94 @@ interface Country {
   label: string;
 }
 
+const countryCodeMap: { [key: number]: string } = {
+  46: "GE", // Абхазия
+  31: "AT", // Австрия
+  55: "AZ", // Азербайджан
+  17: "AD", // Андорра
+  88: "AR", // Аргентина
+  53: "AM", // Армения
+  59: "BH", // Бахрейн
+  57: "BY", // Беларусь
+  20: "BG", // Болгария
+  39: "BR", // Бразилия
+  37: "HU", // Венгрия
+  90: "VE", // Венесуэла
+  16: "VN", // Вьетнам
+  38: "DE", // Германия
+  6: "GR", // Греция
+  54: "GE", // Грузия
+  11: "DO", // Доминикана
+  1: "EG", // Египет
+  30: "IL", // Израиль
+  3: "IN", // Индия
+  7: "ID", // Индонезия
+  29: "JO", // Иордания
+  92: "IR", // Иран
+  14: "ES", // Испания
+  24: "IT", // Италия
+  78: "KZ", // Казахстан
+  40: "KH", // Камбоджа
+  79: "QA", // Катар
+  51: "KE", // Кения
+  15: "CY", // Кипр
+  60: "KG", // Киргизия
+  13: "CN", // Китай
+  10: "CU", // Куба
+  80: "LB", // Ливан
+  27: "MU", // Маврикий
+  36: "MY", // Малайзия
+  8: "MV", // Мальдивы
+  50: "MT", // Мальта
+  23: "MA", // Марокко
+  18: "MX", // Мексика
+  81: "MM", // Мьянма
+  82: "NP", // Непал
+  9: "AE", // ОАЭ
+  64: "OM", // Оман
+  87: "PA", // Панама
+  35: "PT", // Португалия
+  47: "RU", // Россия
+  93: "SA", // Саудовская Аравия
+  28: "SC", // Сейшелы
+  58: "RS", // Сербия
+  25: "SG", // Сингапур
+  43: "SI", // Словения
+  2: "TH", // Таиланд
+  41: "TZ", // Танзания
+  5: "TN", // Тунис
+  4: "TR", // Турция
+  56: "UZ", // Узбекистан
+  26: "PH", // Филиппины
+  32: "FR", // Франция
+  22: "HR", // Хорватия
+  21: "ME", // Черногория
+  12: "LK", // Шри-Ланка
+  70: "KR", // Южная Корея
+  49: "JP", // Япония
+  19: "CZ", // Чехия
+};
+
 export default function NewFlyingCountry() {
   const { setData, countries } = useContext(DataContext);
-  const [selectedCountry, setSelectedCountry] = useState<number>(24);
+  const [selectedCountry, setSelectedCountry] = useState<number | null>(null);
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    if (
-      countries.length > 0 &&
-      !countries.find((country) => country.id === selectedCountry)
-    ) {
-      setSelectedCountry(countries[24].id); // Исправлено
+    if (countries.length > 0) {
+      const countryExists = countries.some(
+        (country) => country.id === selectedCountry
+      );
+      if (!countryExists) {
+        setSelectedCountry(countries[24].id);
+      }
     }
   }, [countries]);
 
   useEffect(() => {
-    setData("param2", selectedCountry);
+    if (selectedCountry !== null) {
+      setData("param2", selectedCountry);
+    }
   }, [selectedCountry, setData]);
 
   const handleCountrySelect = (country: { id: number; label: string }) => {
@@ -32,13 +104,14 @@ export default function NewFlyingCountry() {
 
   const selectedCountryData = countries.find(
     (country: Country) => country.id === selectedCountry
-  ); // Исправлено
+  );
 
   return (
     <Popover
       placement="top"
       isOpen={isOpen}
       onOpenChange={(open) => setIsOpen(open)}
+      key={countries.length}
     >
       <PopoverTrigger className="w-64 h-full bg-white hover:bg-slate-100 rounded-xl !z-0 !scale-100 !opacity-100">
         <Button className="px-4">
@@ -47,6 +120,16 @@ export default function NewFlyingCountry() {
               <span className="text-slate-600 mb-[1px] text-sm">Страна</span>
             )}
             <div className="flex items-center gap-2">
+              {selectedCountryData && (
+                <img
+                  src={`https://flagcdn.com/${
+                    countryCodeMap[selectedCountryData.id]?.toLowerCase() ||
+                    "default"
+                  }.svg`}
+                  alt={selectedCountryData.label}
+                  className="w-6 h-4"
+                />
+              )}
               <h1
                 className={`text-lg ${
                   selectedCountryData
@@ -54,15 +137,17 @@ export default function NewFlyingCountry() {
                     : "text-slate-600"
                 }`}
               >
-                {selectedCountryData ? selectedCountryData.label : ""}
+                {selectedCountryData
+                  ? selectedCountryData.label
+                  : "Выберите страну"}
               </h1>
             </div>
           </div>
         </Button>
       </PopoverTrigger>
 
-      <PopoverContent className="w-[50rem] py-2">
-        <div className="grid grid-cols-4 gap-1 items-start w-full">
+      <PopoverContent className="w-[43rem] py-2 max-h-96 ">
+        <div className="grid grid-cols-3 gap-1 items-start w-full overflow-y-auto scrollbar-custom pr-2">
           {countries.length > 0 ? (
             countries.map((country) => (
               <button
@@ -73,6 +158,16 @@ export default function NewFlyingCountry() {
                 onClick={() => handleCountrySelect(country)}
               >
                 {country.label}
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://flagcdn.com/${
+                      countryCodeMap[country.id]?.toLowerCase() || "default"
+                    }.svg`}
+                    alt={country.label}
+                    className="w-6 h-4"
+                  />
+                  {country.label}
+                </div>
               </button>
             ))
           ) : (

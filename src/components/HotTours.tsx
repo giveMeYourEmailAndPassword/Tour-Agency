2db26006f1d86a53816062cb318d912edc0a7970
unknown <vvvvv8206@gmail.com>
2025-03-10 17:22:17 +0600

    сделал таск с кнопками у HotTours

--- src/components/HotTours.tsx
+++ src/components/HotTours.tsx
@@ -97,24 +97,46 @@ export default function HotTours() {
     <div className="flex flex-col my-14 mx-36 gap-8">
       <div className="flex flex-col gap-3">
         <div className="flex items-end gap-1">
-          <h2 className="text-3xl font-semibold">Горящие туры</h2>
+          <h2 className="text-3xl font-semibold">
+            Горящие туры
+            {selectedCity === "80"
+              ? " из Бишкека"
+              : selectedCity === "60"
+              ? " из Алматы"
+              : ""}
+          </h2>
           <BsFire className="text-3xl text-orange-500" />
         </div>
         <div className="flex gap-2">
           <button
-            className="px-5 py-2 rounded-full bg-slate-200 text-black"
+            className={`px-5 py-2 rounded-full font-semibold
+               ${
+                 selectedCity === ""
+                   ? "text-blue-500 bg-slate-200"
+                   : "text-black bg-slate-100 text-opacity-50"
+               }`}
             onClick={() => setSelectedCity("")}
           >
             Все
           </button>
           <button
-            className="px-5 py-2 rounded-full bg-slate-100 text-black text-opacity-70"
+            className={`px-5 py-2 rounded-full font-semibold
+              ${
+                selectedCity === "80"
+                  ? "text-blue-500 bg-slate-200"
+                  : "text-black bg-slate-100 text-opacity-50"
+              }`}
             onClick={() => setSelectedCity("80")}
           >
             из Бишкека
           </button>
           <button
-            className="px-5 py-2 rounded-full bg-slate-100 text-black text-opacity-70"
+            className={`px-5 py-2 rounded-full font-semibold
+              ${
+                selectedCity === "60"
+                  ? "text-blue-500 bg-slate-200"
+                  : "text-black bg-slate-100 text-opacity-50"
+              }`}
             onClick={() => setSelectedCity("60")}
           >
             из Алматы

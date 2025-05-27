489b5b6d094856d32421a936e6d488454de2cb11
unknown <vvvvv8206@gmail.com>
2025-05-27 20:32:16 +0600

    добавил избранное для пк и мобилки

--- src/components/FloatingControls.tsx
+++ src/components/FloatingControls.tsx
@@ -5,7 +5,10 @@ export default function FloatingControls() {
   return (
     <div className="fixed bottom-8 right-0 flex flex-col items-end gap-2 z-50">
       <ScrollToTopButton />
-      <Favorite />
+
+      <div className="hidden md:block">
+        <Favorite />
+      </div>
     </div>
   );
 }

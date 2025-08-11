3189b7f816be6f7ef4de3ba22ea6c41f1757f95f
vvvvv8206@gmail.com <vvvvv8206@gmail.com>
2025-08-12 00:06:42 +0600

    чекпоинт

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { useState, useEffect, useContext } from "react";
 import Header from "./components/Header";
 import Filters from "./components/Filters";
-import { useState } from "react";
 import FiltersMobile from "./components/FiltersMobile";
 import SearchResults from "./components/SearchResults";
+import { DataContext } from "./components/DataProvider";
 
 export default function App() {
   const [showResults, setShowResults] = useState(false);
+  const { searchTours, tours, params } = useContext(DataContext);
+  const [isFetching, setIsFetching] = useState(false);
+
+  // Функция для проверки готовности параметров
+  const areParamsReady = (params) => {
+    return (
+      params.param1 &&
+      params.param2 &&
+      params.param4?.startDate &&
+      params.param4?.endDate
+    );
+  };
+
+  useEffect(() => {
+    console.log("Проверка параметров:", params);
+    if (areParamsReady(params) && !isFetching) {
+      console.log("Параметры готовы, выполняем запрос");
+      setIsFetching(true); // Устанавливаем состояние, что запрос начался
+      searchTours()
+        .then(() => {
+          console.log("Поиск завершен");
+          setShowResults(true);
+        })
+        .catch((error) => {
+          console.error("Ошибка при выполнении поиска туров:", error);
+        })
+        .finally(() => {
+          setIsFetching(false); // Сбрасываем состояние после завершения запроса
+        });
+    } else {
+      console.log(
+        "Параметры не готовы или запрос уже выполняется, запрос не выполнен"
+      );
+    }
+  }, [params, searchTours]); // Убедитесь, что isFetching не является зависимостью
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -18,7 +54,7 @@ export default function App() {
           <div className="md:block hidden mt-4">
             <div className="flex items-start">
               <Filters />
-              {showResults ? (
+              {showResults && tours.length > 0 ? (
                 <SearchResults />
               ) : (
                 <div className="flex-1 p-8 text-center">

acead7d7ecfd025ece8686d3186718b62f3fb481
vvvvv8206@gmail.com <vvvvv8206@gmail.com>
2025-09-15 13:38:25 +0600

    переархитектура проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,165 +1,40 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { useLocation, useNavigationType } from "react-router-dom";
 import HeaderFilters from "./components/HeaderFilters";
 import Filters from "./components/Filters";
 import FiltersMobile from "./components/FiltersMobile";
-import SearchResults from "./components/SearchResults";
 import { DataContext } from "./components/DataProvider";
-import { Skeleton } from "@heroui/react";
 import { useSearchParams } from "./Hooks/useSearchParams";
 import Header from "./components/Header";
+import HotTours from "./components/HotTours";
 
 export default function App() {
-  const [showResults, setShowResults] = useState(false);
-  const { searchTours, tours, params, searchInProgress } =
-    useContext(DataContext); // добавляем searchInProgress
-  const [isFetching, setIsFetching] = useState(false);
-  const [isInitialSearch, setIsInitialSearch] = useState(true);
-  const navigationType = useNavigationType();
+  const { params } = useContext(DataContext);
   const location = useLocation();
 
   useSearchParams();
-
-  // Функция для проверки готовности параметров
-  const areParamsReady = (params) => {
-    return (
-      params.param1 &&
-      params.param2 &&
-      params.param4?.startDate &&
-      params.param4?.endDate
-    );
-  };
-
-  useEffect(() => {
-    // Если мы вернулись назад и у нас есть туры или поиск в процессе
-    if (navigationType === "POP" && (tours.length > 0 || searchInProgress)) {
-      setShowResults(true);
-      return; // Прерываем выполнение, чтобы не начинать новый поиск
-    }
-
-    // Если это первая загрузка страницы или прямой переход
-    if (
-      isInitialSearch &&
-      areParamsReady(params) &&
-      location.pathname === "/" // Проверяем, что мы на главной странице
-    ) {
-      setIsInitialSearch(false);
-      setIsFetching(true);
-      searchTours()
-        .then(() => {
-          setShowResults(true);
-        })
-        .catch((error) => {
-          console.error("Ошибка при выполнении поиска туров:", error);
-        })
-        .finally(() => {
-          setIsFetching(false);
-        });
-    }
-  }, [
-    isInitialSearch,
-    params,
-    searchTours,
-    navigationType,
-    location.pathname,
-    tours.length,
-    searchInProgress,
-  ]);
 
   return (
     <div className={`min-h-screen flex flex-col md:bg-white bg-gray-100`}>
       <Header />
       <div className="hidden md:block">
-        <HeaderFilters onSearch={() => setShowResults(true)} />
+        <HeaderFilters />
       </div>
 
-      {/* Секция с белым фоном */}
       <div className="w-full mt-1 md:mt-0 md:bg-white bg-gray-100">
         <div className="max-w-[1440px] mx-auto">
-          {/* Фильтры и результаты поиска */}
           <div className="md:block hidden mt-4">
             <div className="flex items-start">
-              <Filters />
-              {showResults && tours.length > 0 ? (
-                <SearchResults />
-              ) : (
-                <div className="ml-2 flex-grow pb-4">
-                  <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-2">
-                    {[...Array(36)].map((_, index) => (
-                      <div
-                        key={index}
-                        className="w-full flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px]"
-                      >
-                        <div className="w-64 flex flex-col gap-2">
-                          <Skeleton className="w-full h-36 rounded" />
-                          <div className="w-full flex flex-col gap-2">
-                            <div className="w-full flex justify-between items-center gap-1">
-                              <div className="flex items-center gap-0.5">
-                                <Skeleton className="w-24 h-4" />
-                              </div>
-                              <Skeleton className="w-24 h-4" />
-                            </div>
-                            <Skeleton className="w-full h-7" />
-                            <Skeleton className="w-3/4 h-5" />
-                          </div>
-                          <div className="w-full flex items-center gap-3 pb-1 border-b border-[#DBE0E5]">
-                            <Skeleton className="w-20 h-6" />
-                          </div>
-                          <div className="w-full flex justify-between items-center">
-                            <Skeleton className="w-20 h-3" />
-                            <div className="flex flex-col items-end gap-1">
-                              <Skeleton className="w-32 h-[11px]" />
-                              <Skeleton className="w-24 h-3" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
+              <div className="ml-2 flex-grow pb-4">
+                <HotTours />
+              </div>
             </div>
           </div>
           <div className="block md:hidden">
             <FiltersMobile />
-            {showResults && tours.length > 0 ? (
-              <SearchResults />
-            ) : (
-              <div className="mx-2 flex-grow">
-                <div className="grid grid-cols-1 gap-2">
-                  {[...Array(12)].map((_, index) => (
-                    <div
-                      key={index}
-                      className="w-full flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px]"
-                    >
-                      <div className="w-full flex flex-col gap-2">
-                        <Skeleton className="w-full h-36 rounded" />
-                        <div className="w-full flex flex-col gap-2">
-                          <div className="w-full flex justify-between items-center gap-1">
-                            <div className="flex items-center gap-0.5">
-                              <Skeleton className="w-24 h-4" />
-                            </div>
-                            <Skeleton className="w-24 h-4" />
-                          </div>
-                          <Skeleton className="w-full h-7" />
-                          <Skeleton className="w-3/4 h-5" />
-                        </div>
-                        <div className="w-full flex items-center gap-3 pb-1 border-b border-[#DBE0E5]">
-                          <Skeleton className="w-20 h-6" />
-                        </div>
-                        <div className="w-full flex justify-between items-center">
-                          <Skeleton className="w-20 h-7" />
-                          <div className="flex flex-col items-end gap-1">
-                            <Skeleton className="w-32 h-4" />
-                            <Skeleton className="w-24 h-4" />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
+            <div className="mx-2 flex-grow">
+              <HotTours />
+            </div>
           </div>
         </div>
       </div>
--- src/components/HotTours.tsx
+++ src/components/HotTours.tsx
@@ -2,16 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import { BsFire } from "react-icons/bs";
-import { GoStarFill } from "react-icons/go";
 import { parse, format, addDays } from "date-fns";
-import { ru } from "date-fns/locale"; // Русская локализация
+import { ru } from "date-fns/locale";
 import { Skeleton } from "@heroui/react";
 import { useState } from "react";
-import { countryCodeMap } from "../constants/countryCodeMap";
 import starFilled from "../assets/star_fill.svg";
-import starOutline from "../assets/star.svg";
-import utensils from "../assets/moon_stars.svg";
-import bed from "../assets/person_luggage.svg";
+import starOutline from "../assets/star_unfill.svg";
+import utensils from "../assets/utensils.svg";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:8000/api";
@@ -21,11 +18,11 @@ const fetchHotTours = async () => {
   return response.data;
 };
 
-// Добавим функцию для обрезки названия отеля после 3-го пробела
+// Функция для обрезки названия отеля после 2-х слов
 const truncateHotelName = (name: string) => {
   const words = name.split(" ");
-  if (words.length > 3) {
-    return words.slice(0, 3).join(" ") + "...";
+  if (words.length > 2) {
+    return words.slice(0, 2).join(" ");
   }
   return name;
 };
@@ -45,12 +42,10 @@ export default function HotTours() {
       )
     : data?.hottours?.tour || [];
 
-  filteredTours;
-
   if (isLoading) {
     return (
-      <div className="flex flex-col my-4 md:my-14 gap-4 md:gap-8 max-w-[1560px] mx-auto px-4 md:px-8 lg:px-12 xl:px-36">
-        <div className="flex flex-col gap-3">
+      <div className="flex-grow pb-4">
+        <div className="flex flex-col gap-3 mb-4">
           <div className="flex items-end gap-1">
             <h2 className="text-2xl md:text-3xl font-medium md:font-semibold">
               Горящие туры
@@ -69,16 +64,14 @@ export default function HotTours() {
             </button>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-3">
-          {[...Array(8)].map((_, index) => (
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-2">
+          {[...Array(12)].map((_, index) => (
             <div
               key={index}
-              className="w-72 flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px]"
+              className="w-full flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px]"
             >
-              <div className="w-64 flex flex-col gap-2">
-                <Skeleton className="w-full h-36 rounded" />
-
+              <div className="w-full flex flex-col gap-2">
+                <Skeleton className="w-full h-44 md:h-36 rounded" />
                 <div className="w-full flex flex-col gap-2">
                   <div className="w-full flex justify-between items-center gap-1">
                     <div className="flex items-center gap-0.5">
@@ -86,21 +79,17 @@ export default function HotTours() {
                     </div>
                     <Skeleton className="w-24 h-4" />
                   </div>
-
                   <Skeleton className="w-full h-7" />
                   <Skeleton className="w-3/4 h-5" />
                 </div>
-
                 <div className="w-full flex items-center gap-3 pb-1 border-b border-[#DBE0E5]">
                   <Skeleton className="w-20 h-6" />
-                  <Skeleton className="w-20 h-6" />
                 </div>
-
                 <div className="w-full flex justify-between items-center">
-                  <Skeleton className="w-20 h-7" />
+                  <Skeleton className="w-20 h-2" />
                   <div className="flex flex-col items-end gap-1">
-                    <Skeleton className="w-32 h-4" />
-                    <Skeleton className="w-24 h-4" />
+                    <Skeleton className="w-32 h-2" />
+                    <Skeleton className="w-24 h-2" />
                   </div>
                 </div>
               </div>
@@ -118,11 +107,8 @@ export default function HotTours() {
   }
 
   const formatDate = (dateString: string) => {
-    // Парсим строку в объект Date
     const date = parse(dateString, "dd.MM.yyyy", new Date());
-
-    // Форматируем дату в нужный формат
-    return format(date, "d MMMM", { locale: ru }); // "24 октября"
+    return format(date, "d MMMM", { locale: ru });
   };
 
   const getEndDate = (startDate: string, nights: number) => {
@@ -130,9 +116,9 @@ export default function HotTours() {
     return format(addDays(date, nights), "d MMMM", { locale: ru });
   };
 
-  // Добавим функцию для определения типа питания
   const getMealType = (meal: string) => {
     const mealTypes: { [key: string]: string } = {
+      "": "Без питания",
       BB: "Завтрак",
       HB: "Полупансион",
       FB: "Полный пансион",
@@ -144,8 +130,8 @@ export default function HotTours() {
   };
 
   return (
-    <div className="flex flex-col md:my-14 my-4 gap-4 md:gap-8 max-w-[1560px] mx-auto px-4 md:px-8 lg:px-12 xl:px-36">
-      <div className="flex flex-col gap-3">
+    <div className="flex-grow pb-4">
+      <div className="flex flex-col gap-3 mb-4">
         <div className="flex items-end gap-1">
           <h2 className="text-2xl md:text-3xl font-medium md:font-semibold">
             Горящие туры
@@ -159,34 +145,31 @@ export default function HotTours() {
         </div>
         <div className="flex md:gap-2 gap-1">
           <button
-            className={`px-5 py-2 rounded-full font-semibold
-               ${
-                 selectedCity === ""
-                   ? "text-blue-500 bg-slate-200"
-                   : "text-black bg-slate-100 text-opacity-50"
-               }`}
+            className={`px-5 py-2 rounded-full font-semibold ${
+              selectedCity === ""
+                ? "text-blue-500 bg-slate-200"
+                : "text-black bg-slate-100 text-opacity-50"
+            }`}
             onClick={() => setSelectedCity("")}
           >
             Все
           </button>
           <button
-            className={`px-5 py-2 rounded-full font-semibold
-              ${
-                selectedCity === "80"
-                  ? "text-blue-500 bg-slate-200"
-                  : "text-black bg-slate-100 text-opacity-50"
-              }`}
+            className={`px-5 py-2 rounded-full font-semibold ${
+              selectedCity === "80"
+                ? "text-blue-500 bg-slate-200"
+                : "text-black bg-slate-100 text-opacity-50"
+            }`}
             onClick={() => setSelectedCity("80")}
           >
             из Бишкека
           </button>
           <button
-            className={`px-5 py-2 rounded-full font-semibold
-              ${
-                selectedCity === "60"
-                  ? "text-blue-500 bg-slate-200"
-                  : "text-black bg-slate-100 text-opacity-50"
-              }`}
+            className={`px-5 py-2 rounded-full font-semibold ${
+              selectedCity === "60"
+                ? "text-blue-500 bg-slate-200"
+                : "text-black bg-slate-100 text-opacity-50"
+            }`}
             onClick={() => setSelectedCity("60")}
           >
             из Алматы
@@ -194,16 +177,16 @@ export default function HotTours() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-2">
         {filteredTours.map((tour: any, index: number) => (
           <div
             key={index}
-            className="w-72 flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px] cursor-pointer"
+            className="w-full flex items-center gap-2.5 p-4 bg-white border border-[#DBE0E5] rounded-[10px] cursor-pointer transition-all duration-300"
             onClick={() => navigate(`/hotel/${tour.hotelcode}/${tour.tourid}`)}
           >
-            <div className="w-64 flex flex-col gap-2">
-              {/* Image */}
-              <div className="w-full h-36 rounded">
+            <div className="w-full flex flex-col gap-2">
+              {/* Изображение */}
+              <div className="w-full h-44 md:h-36 rounded">
                 <img
                   src={
                     tour.hotelpicture
@@ -215,7 +198,7 @@ export default function HotTours() {
                 />
               </div>
 
-              {/* Hotel Info */}
+              {/* Информация об отеле */}
               <div className="w-full flex flex-col gap-2">
                 <div className="w-full flex justify-between items-center gap-1">
                   <div className="flex items-center gap-0.5">
@@ -237,7 +220,9 @@ export default function HotTours() {
                     ))}
                     {tour.hotelrating !== "0" && (
                       <div className="bg-[#FF621F] text-white text-xs font-medium px-1 rounded-[20px] ml-0.5">
-                        {tour.hotelrating}
+                        {tour.hotelrating.length === 1
+                          ? `${tour.hotelrating}.0`
+                          : tour.hotelrating}
                       </div>
                     )}
                   </div>
@@ -246,25 +231,27 @@ export default function HotTours() {
                   </span>
                 </div>
 
-                <h3 className="text-[#2E2E32] text-lg font-bold leading-[1.22]">
-                  {truncateHotelName(tour.hotelname)}
-                </h3>
-                <p className="text-[#6B7280] text-sm font-light leading-[1.29]">
-                  {tour.countryname}, {tour.hotelregionname}
-                </p>
-              </div>
-
-              {/* Tags - оставляем только питание */}
-              <div className="w-full flex items-center pb-1 border-b border-[#DBE0E5]">
-                <div className="flex items-center gap-1">
-                  <img src={utensils} alt="meal" className="w-3.5 h-3.5" />
-                  <span className="text-sm text-[#2E2E32]">
-                    {getMealType(tour.meal)}
-                  </span>
+                <div>
+                  <h3 className="text-[#2E2E32] text-lg font-bold leading-[1.22]">
+                    {truncateHotelName(tour.hotelname)}
+                  </h3>
+                  <p className="text-[#6B7280] text-base leading-[1.29]">
+                    {tour.countryname}, {tour.hotelregionname}
+                  </p>
                 </div>
               </div>
 
-              {/* Price and Dates */}
+              {/* Теги */}
+              <div className="w-full flex items-center gap-3 pb-1 border-b border-[#DBE0E5]">
+                <div className="flex items-center gap-1">
+                  <span className="text-sm text-[#2E2E32]">
+                    {getMealType(tour.meal)}
+                  </span>
+                  <img src={utensils} alt="meal" className="w-3.5 h-3.5" />
+                </div>
+              </div>
+
+              {/* Цена и даты */}
               <div className="w-full flex justify-between items-center">
                 <span className="text-xl font-bold text-[#2E2E32]">
                   {tour.price * 2}
@@ -279,7 +266,7 @@ export default function HotTours() {
                     {formatDate(tour.flydate)} -{" "}
                     {getEndDate(tour.flydate, parseInt(tour.nights))}
                   </span>
-                  <span className="text-xs font-light text-[#6B7280]">
+                  <span className="text-sm text-[#6B7280]">
                     кол-во ночей: {tour.nights}
                   </span>
                 </div>
